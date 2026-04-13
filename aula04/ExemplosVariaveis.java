036d579197923d2d77f165fb337c9a003e22d0f6
Miguel Alves <miguelalvescruz3937@gmail.com>
2026-04-13 09:25:02 -0300

    Aula prática de variáveis referente ao dia 13/04/2026

---
+++ aula04/ExemplosVariaveis.java
@@ -0,0 +1,19 @@
+package aula04;
+
+public class ExemplosVariaveis {
+    public static void main(String[] args) {
+        int n1, n2, n3;
+        float n4;
+        String l1;
+
+        n1 = 2;
+        n2 = 4;
+        n3 = 6;
+        n4 = 6.94f;
+        l1 = "Bababooey";
+
+        System.out.println(n1 + n2 + n3);   //Somas de INT
+        System.out.println(n3 + n4);    //Soma de INT com Float
+
+    }
+}
