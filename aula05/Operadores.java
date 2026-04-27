c31c5a344a6182752de1253085a58ffcd9365a76
Miguel Alves <miguelalvescruz3937@gmail.com>
2026-04-27 09:22:02 -0300

    Add Conversoes and Operadores classes for variable conversion and arithmetic operations

---
+++ aula05/Operadores.java
@@ -0,0 +1,19 @@
+package aula05;
+
+public class Operadores {  //Arquivo destinado para teste de operadores aritméticos 
+  public static void main(String[] args) {
+    //Definição de variáveis:
+    int x = 5;
+    int y = 6;
+    int z = 2;
+
+    //Teste dos operadores:
+    System.out.println(x + y);
+    System.out.println(y - x);
+    System.out.println(x * y);
+    System.out.println(x + y * z);
+    System.out.println(x % z);
+    System.out.println(++x);
+    System.out.println(--y);
+  }
+}
