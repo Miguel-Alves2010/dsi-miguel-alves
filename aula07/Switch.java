0fccbbe44585b48726c231f4483a2d048b62b560
Miguel Alves <miguelalvescruz3937@gmail.com>
2026-05-11 09:22:40 -0300

    Aula referente ao dia 11/05 sobre estrutura de decisão

---
+++ aula07/Switch.java
@@ -0,0 +1,36 @@
+package aula07;
+
+public class Switch {
+    public static void main(String[] args) {
+    
+    int aula = 6;
+    switch (aula) {
+        case 1:
+        System.out.println("Desenvolvimento de sistemas");
+        break;
+
+        case 2: 
+        System.out.println("Desenvolvimento de sistemas");
+        break;
+
+        case 3: 
+        System.out.println("Desenvolvimento de sistemas");
+        break;
+
+        case 4:
+        System.out.println("Ética e cidadania organizacional");
+        break;
+
+        case 5:
+        System.out.println("Português");
+        break;
+
+        case 6:
+        System.out.println("Português");
+        break;
+    }
+
+    
+    }
+}
+
